8d8fd674c21170fd0bd052a7eff361419265accb
xuan-li233 <56431641+xuan-li233@users.noreply.github.com>
2020-04-27 09:18:34 +0200

    Delete quote.js

--- src/js/quote.js
+++
@@ -1,8 +0,0 @@
-/*
-* Objectif : récupérer une citation aléatoire à partir d'une API et l'afficher
-*
-* Étapes :
-* 1- Créer une référence vers les éléments du DOM qu'on va utiliser
-* 2- Récupérer une citation aléatoire à partir de l'API de QuotesOnDesign (https://quotesondesign.com/api/)
-* 3- Afficher la citation
-* */
